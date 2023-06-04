8e3c3f248c17ec1e956e8e65ccbf4955d41dd025
Edward Ho <pythoned77@gmail.com>
2023-06-04 11:08:23 +0800

    NavBar Routes Done

--- blogspot/src/App.js
+++ blogspot/src/App.js
@@ -1,20 +1,21 @@
 import React from "react";
-import { ChakraProvider } from "@chakra-ui/react";
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/home";
 import RecentWorks from "./pages/recentworks";
 import Contact from "./pages/contact";
+import NavBar from "./components/navbar";
 
 const App = () => {
   return (
     <div className="App">
-      <ChakraProvider>
+      <Router>
+        <NavBar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/recentworks" element={<RecentWorks />} />
           <Route path="/contact" element={<Contact />} />
         </Routes>
-      </ChakraProvider>
+      </Router>
     </div>
   );
 };
--- blogspot/src/components/navbar/index.js
+++ blogspot/src/components/navbar/index.js
@@ -1,25 +1,44 @@
 import React from "react";
-import { Menu, MenuButton, Avatar, Box } from "@chakra-ui/react";
+import { Link as RouterLink } from "react-router-dom";
+import {
+  Menu,
+  MenuButton,
+  Avatar,
+  MenuList,
+  MenuItem,
+  Flex,
+} from "@chakra-ui/react";
 import "./index.css";
 
 const NavBar = () => {
-  const pages = ["Recent Works", "Contact"];
+  const links = [
+    { to: "/", label: "home" },
+    { to: "/recentworks", label: "Recent Works" },
+    { to: "/contact", label: "Contact" },
+  ];
 
   return (
     <div>
-      <Box className="menu">
-        <Menu>
-          <MenuButton>Edward Ho</MenuButton>
-          {pages.map((page) => {
-            return <MenuButton className="menuTab">{page}</MenuButton>;
-          })}
+      <Flex className="menu">
+        <Menu className="menuBar">
+          {links.map((link) => (
+            <MenuButton
+              key={link.to}
+              as={RouterLink}
+              to={link.to}
+              className="menuTab"
+            >
+              {link.label}
+            </MenuButton>
+          ))}
+
           <Avatar
             name="Dan Abrahmov"
             src="https://bit.ly/dan-abramov"
             className="avatar"
           />
         </Menu>
-      </Box>
+      </Flex>
     </div>
   );
 };
