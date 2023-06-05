84d89d44f136bb00ba781b18277e4fd2e5d7f6f3
Edward Ho <pythoned77@gmail.com>
2023-06-06 05:55:57 +0800

    added carousel test for recentWorks

--- blogspot/src/pages/recentworks/index.js
+++ blogspot/src/pages/recentworks/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import "./index.css";
 import ContentCard from "../../components/contentCard";
 import ImageCard from "../../components/imageCard";
--- blogspot/src/pages/recentworks/index.test.js
+++ blogspot/src/pages/recentworks/index.test.js
@@ -1,5 +1,5 @@
 import "@testing-library/jest-dom/extend-expect";
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import RecentWorks from "./index.js";
 
 test("image count test", () => {
@@ -15,4 +15,19 @@ test("conditional rendering based on selected card", () => {
   render(<RecentWorks />);
   const selectedCard = screen.getByTestId("card");
   expect(selectedCard).toBeInTheDocument();
+});
+
+test("clicking on the carousel updates the displayed card", () => {
+  const cards = ["card1", "card2", "card3", "card4", "card5"];
+
+  render(<RecentWorks cards={cards} />);
+
+  const intialCard = screen.getByAltText("card1");
+  expect(intialCard).toBeInTheDocument();
+
+  const carousel = screen.getByAltText("recent works");
+  fireEvent.click(carousel);
+
+  const nextCard = screen.getAllByAltText("card2");
+  expect(nextCard).toBeInTheDocument();
 });
